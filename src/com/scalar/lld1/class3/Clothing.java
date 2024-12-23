7b7583cb9432b78f1c9c12e0f2a08ff04d6e24ba
aprabhu <aprabhu@infinera.com>
2024-12-23 21:23:12 +0530

    Comparable changes

--- src/com/scalar/lld1/class3/Clothing.java
+++ src/com/scalar/lld1/class3/Clothing.java
@@ -1,5 +1,18 @@
 package com.scalar.lld1.class3;
 
-public class Clothing {
+public class Clothing extends Item {
     private String size;
+
+    public Clothing(String id, String name, double price, int quantity, String size) {
+        super(id, name, price, quantity);
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
